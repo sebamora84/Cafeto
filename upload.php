45ab5c5ad5e10e22aa08a6745b57d0da05fe231f
sebamora84 <sebamora@gmail.com>
2017-07-07 18:03:45 -0300

    allow upload files

---
+++ upload.php
@@ -0,0 +1,23 @@
+<?php
+// In PHP versions earlier than 4.1.0, $HTTP_POST_FILES should be used instead
+// of $_FILES.
+
+$uploaddir = '/var/www/html/uploads/';
+$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+
+
+$result = move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile);
+
+echo '<pre>';
+if ($result) {
+    echo "File is valid, and was successfully uploaded.\n";
+} else {
+    echo "Possible file upload attack!\n";
+}
+
+echo 'Here is some more debugging info:';
+print_r($_FILES);
+echo $uploadfile;
+print "</pre>";
+
+?>
