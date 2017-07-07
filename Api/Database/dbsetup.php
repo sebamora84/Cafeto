c606b30bcb752be5ef8a9ced7fd0ad70fa9ca61d
sebamora84 <sebamora@gmail.com>
2017-07-07 14:46:46 -0300

    freeze db

--- Api/Database/dbsetup.php
+++ Api/Database/dbsetup.php
@@ -2,5 +2,5 @@
 		require_once '../Database/dbdata.php';
 		R::setup('mysql:host='.$dbhost.';dbname='.$dbname, $username, $password);
 		//R::fancyDebug( TRUE );
-		//R::freeze( true );
+		R::freeze( true );
 ?>
